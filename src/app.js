23280fe9f615a7fe2f261acbc5ed8225ef9baf6f
justinmchase <justin.m.chase@outlook.com>
2014-03-03 13:20:50 -0600

    Only pause and print if a debugger is attached.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-/// <reference path="node.d.ts" />
+if(v8debug) {
+    var readline = require('readline');
 
-var readline = require('readline');
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
 
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-rl.question("Press any key to continue...", function (answer) {
-    rl.close();
-});
+    rl.question("Press any key to continue...", function (answer) {
+        rl.close();
+    });
+}
